8eb3a6a54b05490d775278ee037c6bd22dbb3562
Alif Ramadhani <129699333+alif-ramadhani@users.noreply.github.com>
2023-06-15 10:43:05 +0700

    add src alif

---
+++ src/assets/data/activity.js
@@ -0,0 +1,40 @@
+import acImg01 from "../images/event1.png";
+import acImg02 from "../images/event11.png";
+import acImg03 from "../images/event12.png";
+import acImg04 from "../images/event13.png";
+import acImg05 from "../images/event14.png";
+import acImg06 from "../images/event7.jpg";
+
+const Activity_data = [
+  {
+    id: 1,
+    title: "The services provide for design ",
+    image: acImg01,
+  },
+  {
+    id: 2,
+    title: "Mobile app landing design & maintain",
+    image: acImg02,
+  },
+  {
+    id: 3,
+    title: "Logo design creativity & Application ",
+    image: acImg03,
+  },
+  {
+    id: 4,
+    title: "Mobile app landing design & Services",
+    image: acImg04,
+  },
+  {
+    id: 5,
+    title: "Design for tecnology & services",
+    image: acImg05,
+  },
+  {
+    id: 6,
+    title: "App for tecnology & services",
+    image: acImg06,
+  },
+];
+export default Activity_data;
---
+++ src/assets/data/info.js
@@ -0,0 +1,186 @@
+import infoImg01 from "../images/coba.jpg";
+import infoImg02 from "../images/coba.jpg";
+import infoImg03 from "../images/kay2.jpg";
+import infoImg04 from "../images/coba.jpg";
+import infoImg05 from "../images/kay2.jpg";
+import infoImg06 from "../images/alfamidi.jpg";
+import infoImg07 from "../images/sosial.jpg";
+import infoImg08 from "../images/infomedia1.jpg";
+import infoImg09 from "../images/lady2.jpg";
+import infoImg10 from "../images/seven.jpeg";
+import infoImg11 from "../images/shop.jpg";
+
+const info = [
+  {
+    id: "01",
+    title: "Video Editor",
+    city: "Bogor-Jawa Barat",
+    type: "Mentally impaired",
+    desc: "Ruveetex is a fabric distributor that continues to grow by selling the best quality and most trusted materials in Jabodetabek. It has been around for more than 10 years. Our products are supported by large factories in Indonesia who have experience in having the best quality materials for decades. Our team is ready to serve all the needs of our customers with the best service. Ruveetex prepares hundreds of color choices for customers. We also provide custom color services for those of you who want to make a special color.",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+    photo: infoImg01,
+    featured: true,
+  },
+  {
+    id: "02",
+    title: "Conten Creator",
+    city: "Bogor-Jawa barat",
+    type: "Mentally impaired",
+    desc: "Ruveetex is a fabric distributor that continues to grow by selling the best quality and most trusted materials in Jabodetabek. It has been around for more than 10 years. Our products are supported by large factories in Indonesia who have experience in having the best quality materials for decades. Our team is ready to serve all the needs of our customers with the best service. Ruveetex prepares hundreds of color choices for customers. We also provide custom color services for those of you who want to make a special color.",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+    photo: infoImg02,
+    featured: true,
+  },
+  {
+    id: "03",
+    title: "Web Developer",
+    city: "Bantul-Yogyakarta",
+    type: "All Disability",
+    desc: "Kay Homedecor is natural fiber from Indonesia. We produce many products such as basket, lampshade, rugs, wall decor, and many other things which are made by rattan, water hyacinth, banana, sisal, mendong, and bamboo. Also we can make other materials by request. We have developed over the years. Since 2018, our products have been shipped to around the world such as Turkey, Spain, California, Albania, and many more.",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+    photo: infoImg03,
+    featured: true,
+  },
+  {
+    id: "04",
+    title: "Graphic Design",
+    city: "Bogor -Jawa Barat",
+    type: "Mentally impaired",
+    desc: "Ruveetex is a fabric distributor that continues to grow by selling the best quality and most trusted materials in Jabodetabek. It has been around for more than 10 years. Our products are supported by large factories in Indonesia who have experience in having the best quality materials for decades. Our team is ready to serve all the needs of our customers with the best service. Ruveetex prepares hundreds of color choices for customers. We also provide custom color services for those of you who want to make a special color.",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+
+    photo: infoImg04,
+    featured: true,
+  },
+  {
+    id: "05",
+    title: "Digital Campaign Optimizer",
+    city: "Bantul-Yogyakarta",
+    type: "All Disabilty",
+    desc: "Kay Homedecor is natural fiber from Indonesia. We produce many products such as basket, lampshade, rugs, wall decor, and many other things which are made by rattan, water hyacinth, banana, sisal, mendong, and bamboo. Also we can make other materials by request. We have developed over the years. Since 2018, our products have been shipped to around the world such as Turkey, Spain, California, Albania, and many more.",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+
+    photo: infoImg05,
+    featured: false,
+  },
+  {
+    id: "06",
+    title: "Helper Alfamidi",
+    city: "Tangerang",
+    type: "Mentally impaired",
+    desc: "PT Midi Utama Indonesia Tbk is a retail network that is easily accessible to the wider community. At the end of 2017 the number of the Company's outlets reached 1,444 outlets consisting of 1,396 Alfamidi outlets, 11 Alfamidi super outlets and 37 Lawson outlets spread across several Indonesian islands including the islands of Java, Kalimantan, Sumatra and Sulawesi. The outlet network consists of self-owned outlets and in the form of franchise partnerships with third parties. Supported by more than 19,000 employees, the Company serves millions of customers throughout Indonesia.",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+    photo: infoImg06,
+    featured: false,
+  },
+  {
+    id: "07",
+    title: "Sosial Media Intern",
+    city: "Jakarta",
+    type: "All Disability",
+    desc: "PT. Kreasi Semesta Raya was founded in May 2003 as a company that markets vehicle protection products under the Otogard brand. Otogard is a vehicle protection product that is applied using high quality materials. Developed with the most modern technology and produced by experienced and tested companies. In 2007, PT. Kreasi Semesta Raya is developing its business in the field of Engine Treatment with the concept of: restoring vehicle engine performance quickly, easily and safely. These treatment products include: Injector Cleaner, Engine Flush, Brake Cleaner, Colling System etc",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+    photo: infoImg07,
+    featured: false,
+  },
+  {
+    id: "08",
+    title: "Agent Contact Center",
+    city: "DKI Jakarta",
+    type: "Mentally impaired",
+    desc: "PT Telkom Indonesia (Persero) Tbk (Telkom) is a State-Owned Enterprise (BUMN) engaged in information and communication technology (ICT) services and telecommunications networks in Indonesia. Telkom's majority shareholder is the Government of the Republic of Indonesia with 52.09%, while the remaining 47.91% is controlled by the public. Telkom shares are traded on the Indonesia Stock Exchange (IDX) with the code TLKM and the New York Stock Exchange (NYSE) with the code TLK.",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+    photo: infoImg08,
+    featured: false,
+  },
+  {
+    id: "09",
+    title: "Operator Jahit",
+    city: "Yogyakarta",
+    type: "Physically disabled",
+    desc: "Lady On Point Indonesia (LOP.ID) is an MSME business engaged in women's clothing convection which was established in 2023. Lady On Point Indonesia (LOP.ID) has the slogan Wear Better, Be On Point Each Day!. Which means using the clothes we produce will make every woman's appearance even more perfect with their own version of beauty. We are optimistic that we can produce the highest quality innovative products at affordable prices, because all women have the right to appear On Point",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+    photo: infoImg09,
+    featured: false,
+  },
+  {
+    id: "10",
+    title: "Admin Online Shop",
+    city: "Yogyakarta",
+    type: "Physically disabled",
+    desc: "Purnama Jaya Service is located in Yogyakarta City. The company works in industries such as computer repair and household goods",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+    photo: infoImg11,
+    featured: false,
+  },
+  {
+    id: "11",
+    title: "Social Media Specialist",
+    city: "Yogyakarta",
+    type: "Physically disabled",
+    desc: "SEVEN INC is a holding company in Yogyakarta engaged in the creative industry and manages the fashion brands Crowsdenim, Alphawear, Indonesia Banget, Grenade, and several other well-known brands.",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+    photo: infoImg10,
+    featured: false,
+  },
+  {
+    id: "12",
+    title: "Content Writer",
+    city: "Yogyakarta",
+    type: "Physically disabled",
+    desc: "SEVEN INC is a holding company in Yogyakarta engaged in the creative industry and manages the fashion brands Crowsdenim, Alphawear, Indonesia Banget, Grenade, and several other well-known brands.",
+    reviews: [
+      {
+        name: "jhon doe",
+      },
+    ],
+    photo: infoImg10,
+    featured: false,
+  },
+];
+
+export default info;
